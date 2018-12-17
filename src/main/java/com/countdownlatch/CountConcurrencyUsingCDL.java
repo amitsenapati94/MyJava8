7a8a8545a1e3c6bf446e20768beb5f96874a51f9
amitsenapati94 <amit94@gmail.com>
2018-12-16 21:15:30 -0600

    examples for count down latch

---
+++ src/main/java/com/countdownlatch/CountConcurrencyUsingCDL.java
@@ -0,0 +1,84 @@
+package com.countdownlatch;
+
+import com.model.Employee;
+import com.model.EmployeeGraph;
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class CountConcurrencyUsingCDL {
+
+    final private AtomicLong totalCount = new AtomicLong();
+    final private CountDownLatch latch = new CountDownLatch(1);
+    final private AtomicLong pendingVisits = new AtomicLong();
+    private ExecutorService service;
+
+
+    public static void main(String[] args) {
+
+        Employee emp1 = EmployeeGraph.createEmployeeGraph();
+
+        CountConcurrencyUsingCDL countConcurrencyUsingCDL = new CountConcurrencyUsingCDL();
+
+        try {
+            System.out.println(countConcurrencyUsingCDL.getTotalCount(emp1));
+        } catch (InterruptedException e) {
+
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+
+
+    private long getTotalCount(final Employee emp) throws InterruptedException {
+
+        service = Executors.newFixedThreadPool(100);
+        pendingVisits.incrementAndGet();
+        try {
+            updateTotalCount(emp);
+            latch.await(100, TimeUnit.SECONDS);
+            return totalCount.longValue();
+        }finally {
+            service.shutdown();
+        }
+
+    }
+
+    private void updateTotalCount(final Employee emp) {
+        long count = 0;
+        if(emp.isAdvisor()) {
+            count = emp.getAdvisorCount();
+        }else {
+            final List<Employee> childrenList = emp.getEmployeeList();
+            if (childrenList != null) {
+                for (Employee child : childrenList) {
+                    if(child.isAdvisor()) {
+                        count += child.getAdvisorCount();
+                    }else {
+                        pendingVisits.incrementAndGet();
+                        service.execute(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                updateTotalCount(child);
+
+                            }
+                        });
+                    }
+                }
+            }
+        }
+
+        totalCount.addAndGet(count);
+        if(pendingVisits.decrementAndGet() == 0) {
+            latch.countDown();
+        }
+    }
+}
--- src/main/java/com/fpij/FluentMailer.java
+++ src/main/java/com/fpij/FluentMailer.java
@@ -5,14 +5,31 @@ import java.util.function.Consumer;
 public class FluentMailer {
   private FluentMailer() {}
   
-  public FluentMailer from(final String address) { /*... */; return this; }
-  public FluentMailer to(final String address)   { /*... */; return this; }
-  public FluentMailer subject(final String line) { /*... */; return this; }
-  public FluentMailer body(final String message) { /*... */; return this; }
+  public FluentMailer from(final String address) {
+    /*... */;
+    //this.from(address);
+    return this;
+  }
+  public FluentMailer to(final String address)   {
+    /*... */;
+    //this.to(address);
+    return this;
+  }
+  public FluentMailer subject(final String line) {
+    /*... */;
+    //this.subject(line);
+    return this;
+  }
+  public FluentMailer body(final String message) {
+    /*... */;
+    //this.body(message);
+    return this;
+  }
   
   public static void send(final Consumer<FluentMailer> block) { 
     final FluentMailer mailer = new FluentMailer();
-    block.accept(mailer); 
+    block.accept(mailer);
+    System.out.println(mailer);
     System.out.println("sending..."); 
   }
 
