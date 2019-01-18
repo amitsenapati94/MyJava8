018442d380d97da87c13bda33e852c4db9c24d31
amitsenapati94 <amit94@gmail.com>
2019-01-17 22:33:38 -0600

    power of 2

---
+++ src/main/java/com/power/Power.java
@@ -0,0 +1,20 @@
+package com.power;
+
+public class Power {
+
+    public static void main(String args[]){
+
+        int i = 64;
+
+        while (i%2==0)
+        {
+            i/=2;
+        }
+
+        if(i==1)
+        {
+            System.out.println("i is power of 2");
+        }
+
+    }
+}
