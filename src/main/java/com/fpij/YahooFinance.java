3158c8cfca7d3cc68a8d95a578103caae69a99df
amitsenapati94 <amit94@gmail.com>
2019-01-17 22:29:20 -0600

    map reduce example

---
+++ src/main/java/com/fpij/YahooFinance.java
@@ -0,0 +1,20 @@
+package com.fpij;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+public class YahooFinance {
+
+    public static BigDecimal getPrice(final String ticker) {
+
+        Map<String,BigDecimal> map = new HashMap<>();
+
+        map.put("AMD",new BigDecimal(1000));
+        map.put("HPQ",new BigDecimal(450));
+        map.put("IBM",new BigDecimal(450));
+
+
+        return map.get(ticker);
+    }
+}
