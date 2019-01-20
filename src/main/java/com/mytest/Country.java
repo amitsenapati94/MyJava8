0e811ec7739b4f4d89a296d1ee587608037160e0
amitsenapati94 <amit94@gmail.com>
2019-01-20 17:59:59 -0600

    group by example

---
+++ src/main/java/com/mytest/Country.java
@@ -0,0 +1,53 @@
+package com.mytest;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+//https://docs.oracle.com/javase/8/docs/api/java/util/stream/Collectors.html
+
+public class Country {
+
+    public enum Continent {ASIA, EUROPE, NA}
+
+    String name;
+    Continent region;
+
+    public Country (String na, Continent reg) {
+        name = na;
+        region = reg;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Continent getRegion() {
+        return region;
+    }
+
+    public void setRegion(Continent region) {
+        this.region = region;
+    }
+
+    public static void main(String[] args) {
+
+
+        List<Country> couList = Arrays.asList (
+                new Country ("Japan", Country.Continent.ASIA),
+                new Country ("USA", Country.Continent.NA),
+                new Country ("Italy", Country.Continent.EUROPE),
+                new Country ("Germany", Country.Continent.EUROPE));
+
+        Map<Continent, List<String>> regionNames =
+                couList.stream ()
+                        .collect(Collectors.groupingBy(Country::getRegion, Collectors.mapping(Country::getName, Collectors.toList())));
+
+        System.out.println(regionNames);
+    }
+}
