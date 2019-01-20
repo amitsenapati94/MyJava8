1d4b5160368677ce06c19f913dab09eab49bae03
amitsenapati94 <amit94@gmail.com>
2019-01-20 17:58:52 -0600

    peek example

---
+++ src/main/java/com/mytest/PeekExample.java
@@ -0,0 +1,30 @@
+package com.mytest;
+
+import java.util.stream.IntStream;
+
+
+
+public class PeekExample {
+
+    //same stream can't be operated on twice.
+    //Exception in thread "main" java.lang.IllegalStateException: stream has already been operated upon or closed
+
+    public static void main(String[] args) {
+        // Creating a stream of integers
+        IntStream stream = IntStream.range(2, 10);
+
+        // performing action sum on elements of
+        // given range and storing the result in sum
+        int sum = stream.peek(System.out::println).sum();
+
+        // Displaying the result of action performed
+        System.out.println("sum is : " + sum);
+
+        // performing action count on elements of
+        // given range and storing the result in Count
+        long Count = stream.peek(System.out::println).count();
+
+        // Displaying the result of action performed
+        System.out.println("count : " + Count);
+    }
+}
