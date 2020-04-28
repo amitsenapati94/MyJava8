a1c08bc2c9503cd0ba27cada1fbeaa4168d66ceb
amit senapati <amit94@gmail.com>
2020-04-27 23:07:15 -0500

    concurrent hash map

---
+++ src/main/java/com/concurrenthashmap/ConcurrentHashMapTest.java
@@ -0,0 +1,53 @@
+//https://www.journaldev.com/122/concurrenthashmap-in-java
+
+package com.concurrenthashmap;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConcurrentHashMapTest {
+
+    public static void main(String[] args) {
+
+        Map<String,String> map = new HashMap<>();
+        map.put("1","1");
+        map.put("2","1");
+        map.put("3","1");
+        map.put("4","1");
+        map.put("5","1");
+        System.out.println("hashmap before interation: "+map);
+
+        Iterator<String> it = map.keySet().iterator();
+
+        while(it.hasNext()){
+            String key = it.next();
+            if(key.equals("3")){
+                map.put("31","31");
+                break;
+            }
+        }
+        System.out.println("hashmap after interation: "+map);
+
+
+        Map<String,String> cmap = new ConcurrentHashMap<>();
+        cmap.put("1","1");
+        cmap.put("2","1");
+        cmap.put("3","1");
+        cmap.put("4","1");
+        cmap.put("5","1");
+        System.out.println("ConcurrentHashMap before interation: "+cmap);
+
+        Iterator<String> itc = cmap.keySet().iterator();
+
+        while(itc.hasNext()){
+            String key = itc.next();
+            if(key.equals("3")){
+                cmap.put("31","31");
+            }
+        }
+        System.out.println("ConcurrentHashMap before interation: "+cmap);
+
+    }
+}
